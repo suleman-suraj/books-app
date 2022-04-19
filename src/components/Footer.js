5c0adffccdc59bfa2f7cc9b6350a959b45dd4a0a
suleman-suraj <69476432+suleman-suraj@users.noreply.github.com>
2022-04-19 11:17:46 +0000

    added some data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,16 @@ function Footer() {
     
 
     return (
-        <footer className='page-footer'>
-            <div className='footer-copyright'>
-                <div className='container'>
-                    &copy; Books 2022
-                </div>
-                    <a href="#" className='gre-text text-lighten-4 right'>Premest</a>
-            </div>
-        </footer>
-    )
+      <footer className="page-footer">
+        <div className="footer-copyright">
+          <div className="container">
+            &copy; Books 2022
+            <a href="#!" className="gre-text text-lighten-4 right">
+              PreMest
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
 }
 export default Footer
